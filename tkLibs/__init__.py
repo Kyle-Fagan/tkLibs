01e911926d37fa981fd7703f751ff91f052313e2
Kyle Fagan <kylfagan@yahoo.com>
2017-08-22 09:58:28 -0500

    Add import of new widgets.

--- tkLibs/__init__.py
+++ tkLibs/__init__.py
@@ -1,7 +1,11 @@
-__all__ = ['autoScrollbar', 'button', 'combobox', 'listbox', 'window']
+__all__ = ['autoScrollbar', 'button', 'combobox', 'entry', 'frame', 'label', 'listbox', 'toplevel', 'window']
 
 from .autoScrollbar import autoScrollbar
 from .button import button
 from .combobox import combobox
+from .entry import entry
+from .frame import frame
+from .label import label
 from .listbox import listbox
+from .toplevel import toplevel
 from .window import window
